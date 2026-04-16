5c360a941f08c9f77e502d18f207cb37fa87d233
sanjaim22 <sanjai.m2023@vitstudent.ac.in>
2026-04-16 16:56:04 +0530

    Create CalculatorTest.java

---
+++ CalculatorTest.java
@@ -0,0 +1,11 @@
+package com.example;
+import static org.junit.Assert.assertEquals;import org.junit.Test;
+public class CalculatorTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() { assertEquals(5, calc.add(2, 3)); }
+    
+    @Test
+    public void testSubtract() { assertEquals(1, calc.subtract(3, 2)); }
+}
